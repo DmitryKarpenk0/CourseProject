1c487313bea211e4a0f9c20c9a43bab0e73b72b2
Dmitry <karpenko.dima.s@mail.ru>
2023-05-19 21:38:54 +0300

    Now Value is an array of type Value_T

--- LSMTree.h
+++ LSMTree.h
@@ -12,12 +12,12 @@ using namespace std;
 template <typename Key_T, typename Value_T>
 class LSMTree {
  public:
-  LSMTree(bool is_filter = 1, bool is_bin_search = 1);
+  LSMTree(int value_size = 1, bool is_filter = 1, bool is_bin_search = 1);
   ~LSMTree() = default;
 
-  void add(Key_T key, Value_T value);
+  void add(Key_T key, Value_T* value);
 
-  Value_T get(Key_T key);
+  Value_T* get(Key_T key);
 
  private:
   // Максимальный размер хранящихся данных в оперативной памяти - 100 Мб
@@ -25,9 +25,10 @@ class LSMTree {
 
   bool is_bloom_filter;
   bool is_bin_search;
+  int value_size;
 
   // Данные в оперативной памяти, хеш-таблица
-  unordered_map<Key_T, Value_T> mem_table;
+  unordered_map<Key_T, Value_T*> mem_table;
 
   // Проверка, достигнут ли предел хранящихся данных в оперативной памяти
   bool is_memory_limit();
@@ -51,12 +52,13 @@ class LSMTree {
 
 // Реализация
 
-// мб не нужен и оставить дефолтным?
 template <typename Key_T, typename Value_T>
-LSMTree<Key_T, Value_T>::LSMTree(bool is_filter, bool is_bin_search) {
+LSMTree<Key_T, Value_T>::LSMTree(int value_size, bool is_filter,
+                                 bool is_bin_search) {
+  this->value_size = value_size;
   is_bloom_filter = is_filter;
   this->is_bin_search = is_bin_search;
-  mem_table = unordered_map<Key_T, Value_T>();
+  mem_table = unordered_map<Key_T, Value_T*>();
   file_system_data = unordered_map<size_t, pair<Key_T, Key_T>>();
 }
 
@@ -83,7 +85,7 @@ string LSMTree<Key_T, Value_T>::get_filename(size_t file_number,
 }
 
 template <typename Key_T, typename Value_T>
-void LSMTree<Key_T, Value_T>::add(Key_T key, Value_T value) {
+void LSMTree<Key_T, Value_T>::add(Key_T key, Value_T* value) {
   // Проверка, нет ли необходимости сгрузить данные на жёсткий диск (при
   // превышении допустимого объёма хранения данных в оперативной памяти)
   if (is_memory_limit()) {
@@ -91,12 +93,12 @@ void LSMTree<Key_T, Value_T>::add(Key_T key, Value_T value) {
     // Добавляем все ключи в фильтр Блюма
     if (is_bloom_filter) {
       bloom_filters.push_back(
-          BloomFilter<Key_T>(THRESHOLD / (sizeof(Key_T) + sizeof(Value_T))));
+          BloomFilter<Key_T>(THRESHOLD / (sizeof(Key_T) + sizeof(Value_T)*value_size)));
       add_to_bloom_filter();
     }
     // ----
     // Сортируем элементы key-value по ключу, удаляем хеш-таблицу
-    vector<pair<Key_T, Value_T>> elems(mem_table.begin(), mem_table.end());
+    vector<pair<Key_T, Value_T*>> elems(mem_table.begin(), mem_table.end());
     mem_table.clear();
     sort(elems.begin(), elems.end());
 
@@ -115,7 +117,8 @@ void LSMTree<Key_T, Value_T>::add(Key_T key, Value_T value) {
     fstream f_value(filename_value, ios::out | ios::binary);
     for (auto &elem : elems) {
       f_key.write((char *)&elem.first, sizeof(Key_T));
-      f_value.write((char *)&elem.second, sizeof(Value_T));
+      f_value.write((char *)elem.second, sizeof(Value_T)*value_size);
+      delete[] elem.second;
     }
     f_key.close();
     f_value.close();
@@ -174,7 +177,7 @@ size_t LSMTree<Key_T, Value_T>::search_linear(size_t file_number, Key_T key) {
 }
 
 template <typename Key_T, typename Value_T>
-Value_T LSMTree<Key_T, Value_T>::get(Key_T key) {
+Value_T* LSMTree<Key_T, Value_T>::get(Key_T key) {
   auto elem = mem_table.find(key);
   if (elem == mem_table.end()) {
     // ----
@@ -204,12 +207,14 @@ Value_T LSMTree<Key_T, Value_T>::get(Key_T key) {
           string filename = get_filename(i, true);
           fstream f(filename, ios::in | ios::binary);
           f.seekg(value_idx * sizeof(Value_T));
-          Value_T value;
-          f.read((char *)&value, sizeof(Value_T));
-          if (value != NULL) {
-            return value;
-          } else
-            continue;
+          Value_T* value = new Value_T[value_size];
+          f.read((char *)value, sizeof(Value_T)*value_size);
+          for (int i = 0; i < value_size; i++) {
+            if (value[i] != NULL) {
+              return value;
+            }
+          }
+          continue;
         } else
           continue;
       } else
@@ -224,6 +229,6 @@ Value_T LSMTree<Key_T, Value_T>::get(Key_T key) {
 
 template <typename Key_T, typename Value_T>
 bool LSMTree<Key_T, Value_T>::is_memory_limit() {
-  return (mem_table.size() + 1) * (sizeof(Key_T) + sizeof(Value_T)) >=
+  return (mem_table.size() + 1) * (sizeof(Key_T) + sizeof(Value_T)*value_size) >=
          THRESHOLD;
 }
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <cmath>
 #include <ctime>
 #include <functional>
@@ -10,33 +11,62 @@
 
 using namespace std;
 
+#define KEY_TYPE unsigned int
+#define VALUE_TYPE unsigned int
+
 int main() {
-   const int KEYS_ADD = 900000;
-   const int KEYS_MAX = 1000000;
-   const bool BLOOM_FILTER = false;
+  const KEY_TYPE KEYS_ADD = 100000000;
+  const KEY_TYPE KEYS_MAX = 1000000000;
+  const VALUE_TYPE VALUE_MAX = numeric_limits<VALUE_TYPE>::max();
+  const int VALUE_ARRAY_LENGTH = 15;
+  const bool BLOOM_FILTER = true;
+  const bool BIN_SEARCH = true;
+  random_device rd;
+  default_random_engine generator(rd());
+  std::uniform_int_distribution<KEY_TYPE> key_distribution(1, KEYS_MAX);
+  std::uniform_int_distribution<VALUE_TYPE> value_distribution(1, VALUE_MAX);
 
-   random_device rd;
-   default_random_engine generator(rd());
-   std::uniform_int_distribution<int> distribution(1, KEYS_MAX);
+  LSMTree<KEY_TYPE, VALUE_TYPE> lsm(VALUE_ARRAY_LENGTH, BLOOM_FILTER, BIN_SEARCH);
 
-   LSMTree<int, int> lsm(BLOOM_FILTER);
+  auto add_start = clock();
+  for (VALUE_TYPE i = 1; i <= KEYS_ADD; i++) {
+    VALUE_TYPE *tmp = new VALUE_TYPE[VALUE_ARRAY_LENGTH];
+    for (int i = 0; i < VALUE_ARRAY_LENGTH; i++) {
+      tmp[i] = value_distribution(generator);
+    }
+    lsm.add(key_distribution(generator), tmp);
+  }
+  auto add_end = clock();
+  cout << "Adding time (s): " << (double)(add_end - add_start) / CLOCKS_PER_SEC
+       << endl;
 
-   auto add_start = clock();
-   for (int i = 1; i <= KEYS_ADD; i++) {
-     lsm.add(distribution(generator), i);
-   }
-   auto add_end = clock();
-   cout << "Adding time (s): " << (double)(add_end - add_start) / CLOCKS_PER_SEC
-        << endl;
+  auto get_start = clock();
+  for (int i = 1; i <= 100000; i++) {
+    VALUE_TYPE* a = lsm.get(key_distribution(generator));
+  }
+  auto get_end = clock();
+  cout << "Getting elements time (s): "
+       << (double)(get_end - get_start) / CLOCKS_PER_SEC << endl;
+  
 
-   auto get_start = clock();
-   for (int i = 1; i <= KEYS_MAX; i++) {
-     int a = lsm.get(i);
-   }
-   auto get_end = clock();
-   cout << "Getting elements time (s): "
-        << (double)(get_end - get_start) / CLOCKS_PER_SEC << endl;
-        
+    
+  /*int a[2] = {1, 2};
+
+fstream fout("test.bin", ios::out | ios::binary);
+cout << sizeof(a) << endl;
+fout.write((char *)a, 2 * sizeof(int));
+int b[2];
+for (int i = 0; i < 2; i++) {
+  cout << b[i] << endl;
+}
+fout.close();
+fstream fin("test.bin", ios::in | ios::binary);
+fin.seekg(0);
+fin.read((char *)b, sizeof(b));
+fin.close();
+for (int i = 0; i < 2; i++) {
+  cout << b[i] << endl;
+}*/
 
   return 0;
 }
