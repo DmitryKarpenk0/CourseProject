8101b69bb3365f0852ad02cc3306f094301339a6
Dmitry <karpenko.dima.s@mail.ru>
2023-05-22 13:40:40 +0300

    Added option to make lsmt without bloom filter, where all keys stoed in ram

--- LSMTree.h
+++ LSMTree.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <cstdio>
 #include <fstream>
 #include <unordered_map>
 #include <vector>
@@ -15,9 +16,9 @@ class LSMTree {
   LSMTree(int value_size = 1, bool is_filter = 1, bool is_bin_search = 1);
   ~LSMTree() = default;
 
-  void add(Key_T key, Value_T* value);
+  void add(Key_T key, Value_T *value);
 
-  Value_T* get(Key_T key);
+  Value_T *get(Key_T key);
 
  private:
   // Максимальный размер хранящихся данных в оперативной памяти - 100 Мб
@@ -28,20 +29,29 @@ class LSMTree {
   int value_size;
 
   // Данные в оперативной памяти, хеш-таблица
-  unordered_map<Key_T, Value_T*> mem_table;
+  unordered_map<Key_T, Value_T *> mem_table;
+
+  // Храним все ключи в оперативной памяти в случае, когда не используем фильтр
+  // Блюма
+  vector<Key_T> keys;
 
   // Проверка, достигнут ли предел хранящихся данных в оперативной памяти
   bool is_memory_limit();
+
+  // ПРИ ОТСУТСТВИИ ФИЛЬТРА БЛЮМА
+  // Сливаем имеющийся на жёстком диске файл с новыми данными из оперативной
+  // памяти
+  void merge(vector<pair<Key_T, Value_T *>> &new_data);
 
   // Фильтр Блюма
   vector<BloomFilter<Key_T>> bloom_filters;
   void add_to_bloom_filter();
 
   // Бинарный поиск по файлу с ключами
-  size_t search_bin(size_t file_number, Key_T key);
+  int search_bin(size_t file_number, Key_T key);
 
   // Линейный поиск по файлу с ключами
-  size_t search_linear(size_t file_number, Key_T key);
+  int search_linear(size_t file_number, Key_T key);
 
   // Метаданные о диапазонах ключей и файловой системе
   unordered_map<size_t, pair<Key_T, Key_T>> file_system_data;
@@ -58,8 +68,12 @@ LSMTree<Key_T, Value_T>::LSMTree(int value_size, bool is_filter,
   this->value_size = value_size;
   is_bloom_filter = is_filter;
   this->is_bin_search = is_bin_search;
-  mem_table = unordered_map<Key_T, Value_T*>();
-  file_system_data = unordered_map<size_t, pair<Key_T, Key_T>>();
+  mem_table = unordered_map<Key_T, Value_T *>();
+  if (is_bloom_filter) {
+    file_system_data = unordered_map<size_t, pair<Key_T, Key_T>>();
+  } else {
+    keys = vector<Key_T>();
+  }
 }
 
 template <typename Key_T, typename Value_T>
@@ -85,143 +99,238 @@ string LSMTree<Key_T, Value_T>::get_filename(size_t file_number,
 }
 
 template <typename Key_T, typename Value_T>
-void LSMTree<Key_T, Value_T>::add(Key_T key, Value_T* value) {
+void LSMTree<Key_T, Value_T>::merge(vector<pair<Key_T, Value_T *>> &new_data) {
+  string filename_old = get_filename(0, true);
+  fstream f_old(filename_old, ios::in | ios::binary);
+
+  string filename_new = "tmp.bin";
+  fstream f_new(filename_new, ios::out | ios::binary);
+
+  size_t mem_data_size = new_data.size();
+  size_t file_data_size = keys.size();
+
+  size_t i, j;
+  for (i = 0, j = 0; i < mem_data_size && j < file_data_size;) {
+    if (new_data[i].first < keys[j]) {
+      f_new.write((char *)new_data[i].second, sizeof(Value_T) * value_size);
+      keys.insert(next(keys.begin(), j++), new_data[i++].first);
+      file_data_size++;
+      delete[] new_data[i].second;
+    } else {
+      Value_T *value = new Value_T[value_size];
+      f_old.read((char *)value, sizeof(Value_T) * value_size);
+
+      f_new.write((char *)value, sizeof(Value_T) * value_size);
+      delete[] value;
+      j++;
+    }
+  }
+
+  if (j == file_data_size) {
+    for (; i < mem_data_size; i++) {
+      f_new.write((char *)new_data[i].second, sizeof(Value_T) * value_size);
+      keys.push_back(new_data[i].first);
+      delete[] new_data[i].second;
+    }
+  } else {
+    for (; j < file_data_size; j++) {
+      Value_T *value = new Value_T[value_size];
+      f_old.read((char *)value, sizeof(Value_T) * value_size);
+      f_new.write((char *)value, sizeof(Value_T) * value_size);
+      delete[] value;
+    }
+  }
+
+  f_old.close();
+  f_new.close();
+  remove(filename_old.c_str());
+  rename(filename_new.c_str(), filename_old.c_str());
+}
+
+template <typename Key_T, typename Value_T>
+void LSMTree<Key_T, Value_T>::add(Key_T key, Value_T *value) {
   // Проверка, нет ли необходимости сгрузить данные на жёсткий диск (при
   // превышении допустимого объёма хранения данных в оперативной памяти)
   if (is_memory_limit()) {
     // ----
     // Добавляем все ключи в фильтр Блюма
     if (is_bloom_filter) {
-      bloom_filters.push_back(
-          BloomFilter<Key_T>(THRESHOLD / (sizeof(Key_T) + sizeof(Value_T)*value_size)));
+      bloom_filters.push_back(BloomFilter<Key_T>(
+          THRESHOLD / (sizeof(Key_T) + sizeof(Value_T) * value_size)));
       add_to_bloom_filter();
+
+      // ----
+      // Сортируем элементы key-value по ключу, удаляем хеш-таблицу
+      vector<pair<Key_T, Value_T *>> elems(mem_table.begin(), mem_table.end());
+      mem_table.clear();
+      sort(elems.begin(), elems.end());
+
+      // ----
+      // Сгружаем данные на жёсткий диск
+      size_t curr_file_number = file_system_data.size();
+      string filename_key = get_filename(curr_file_number, false);
+      string filename_value = get_filename(curr_file_number, true);
+
+      // Добавляем метаданные о новом файле, его номер и диапазон ключей
+      file_system_data.insert(
+          {curr_file_number,
+           make_pair(elems.front().first, elems.back().first)});
+
+      // Пишем в файл
+      fstream f_key(filename_key, ios::out | ios::binary);
+      fstream f_value(filename_value, ios::out | ios::binary);
+      for (auto &elem : elems) {
+        f_key.write((char *)&elem.first, sizeof(Key_T));
+        f_value.write((char *)elem.second, sizeof(Value_T) * value_size);
+        delete[] elem.second;
+      }
+      f_key.close();
+      f_value.close();
+      elems.clear();
+    } else {
+      vector<pair<Key_T, Value_T *>> elems(mem_table.begin(), mem_table.end());
+      mem_table.clear();
+      sort(elems.begin(), elems.end());
+      merge(elems);
+      elems.clear();
     }
-    // ----
-    // Сортируем элементы key-value по ключу, удаляем хеш-таблицу
-    vector<pair<Key_T, Value_T*>> elems(mem_table.begin(), mem_table.end());
-    mem_table.clear();
-    sort(elems.begin(), elems.end());
-
-    // ----
-    // Сгружаем данные на жёсткий диск
-    size_t curr_file_number = file_system_data.size();
-    string filename_key = get_filename(curr_file_number, false);
-    string filename_value = get_filename(curr_file_number, true);
-
-    // Добавляем метаданные о новом файле, его номер и диапазон ключей
-    file_system_data.insert(
-        {curr_file_number, make_pair(elems.front().first, elems.back().first)});
-
-    // Пишем в файл
-    fstream f_key(filename_key, ios::out | ios::binary);
-    fstream f_value(filename_value, ios::out | ios::binary);
-    for (auto &elem : elems) {
-      f_key.write((char *)&elem.first, sizeof(Key_T));
-      f_value.write((char *)elem.second, sizeof(Value_T)*value_size);
-      delete[] elem.second;
-    }
-    f_key.close();
-    f_value.close();
-    elems.clear();
   }
   mem_table.insert({key, value});
 }
 
 template <typename Key_T, typename Value_T>
-size_t LSMTree<Key_T, Value_T>::search_bin(size_t file_number, Key_T key) {
-  // ----
-  // Поиск по файлу с ключами, алгоритм бинарного поиска
-  string filename = get_filename(file_number, false);
-  fstream f(filename, ios::in | ios::binary);
+int LSMTree<Key_T, Value_T>::search_bin(size_t file_number, Key_T key) {
+  if (is_bloom_filter) {
+    // ----
+    // Поиск по файлу с ключами, алгоритм бинарного поиска
+    string filename = get_filename(file_number, false);
+    fstream f(filename, ios::in | ios::binary);
 
-  f.seekg(0, ios::end);
-  size_t l = 0, r = f.tellg() / sizeof(Key_T), k;
-  while (l < r) {
-    k = (l + r) / 2;
+    f.seekg(0, ios::end);
+    size_t l = 0, r = f.tellg() / sizeof(Key_T), k;
+    while (l < r) {
+      k = (l + r) / 2;
+      Key_T tmp;
+      f.seekg(k * (sizeof(Key_T)));
+      f.read((char *)&tmp, sizeof(Key_T));
+      if (tmp == key) {
+        size_t idx = f.tellg() / sizeof(Key_T) - 1;
+        f.close();
+        return idx;
+      } else if (tmp < key) {
+        l = k + 1;
+      } else {
+        r = k;
+      }
+    }
+    f.close();
+  } else {
+    size_t l = 0, r = keys.size(), k;
+    while (l < r) {
+      k = (l + r) / 2;
+      if (keys[k] == key) {
+        return k;
+      } else if (keys[k] < key) {
+        l = k + 1;
+      } else {
+        r = k;
+      }
+    }
+  }
+
+  return -1;
+}
+
+template <typename Key_T, typename Value_T>
+int LSMTree<Key_T, Value_T>::search_linear(size_t file_number, Key_T key) {
+  if (is_bloom_filter) {
+    string filename = get_filename(file_number, false);
+    fstream f(filename, ios::in | ios::binary);
     Key_T tmp;
-    f.seekg(k * (sizeof(Key_T)));
-    f.read((char *)&tmp, sizeof(Key_T));
-    if (tmp == key) {
-      size_t idx = f.tellg() / sizeof(Key_T) - 1;
-      f.close();
-      return idx;
-    } else if (tmp < key) {
-      l = k + 1;
-    } else {
-      r = k;
+    f.seekg(0, SEEK_END);
+    long length = f.tellg() / sizeof(Key_T);
+    f.seekg(0);
+    for (long i = 0; i < length; i++) {
+      f.read((char *)&tmp, sizeof(Key_T));
+      if (tmp == key) {
+        f.close();
+        return i;
+      }
+    }
+    f.close();
+  } else {
+    for (int i = 0; i < keys.size(); i++) {
+      if (keys[i] == key) return i;
     }
   }
-  f.close();
-
-  // TODO: вернуть exception
-  return NULL;
+  return -1;
 }
 
 template <typename Key_T, typename Value_T>
-size_t LSMTree<Key_T, Value_T>::search_linear(size_t file_number, Key_T key) {
-  string filename = get_filename(file_number, false);
-  fstream f(filename, ios::in | ios::binary);
-  Key_T tmp;
-  f.seekg(0, SEEK_END);
-  long length = f.tellg() / sizeof(Key_T);
-  f.seekg(0);
-  for (long i = 0; i < length; i++) {
-    f.read((char *)&tmp, sizeof(Key_T));
-    if (tmp == key) {
-      f.close();
-      return i;
-    }
-  }
-  f.close();
-  return NULL;
-}
-
-template <typename Key_T, typename Value_T>
-Value_T* LSMTree<Key_T, Value_T>::get(Key_T key) {
+Value_T *LSMTree<Key_T, Value_T>::get(Key_T key) {
   auto elem = mem_table.find(key);
   if (elem == mem_table.end()) {
-    // ----
-    // Элемента нет в оперативной памяти. Проверяем фильтр Блюма
-    for (size_t i = 0; i < file_system_data.size(); i++) {
-      bool flag = true;
+    if (is_bloom_filter) {
       // ----
-      // Если хотя бы один из элементов фильтра Блюма, соответствующий данному
-      // ключу равен нулю, то этого элемента нет среди данных
-      if (is_bloom_filter) {
-        flag = bloom_filters[i].possibly_contains(key);
-      }
-      if (flag) {
+      // Элемента нет в оперативной памяти. Проверяем фильтр Блюма
+      for (size_t i = 0; i < file_system_data.size(); i++) {
+        bool flag = true;
         // ----
-        // Если проверка в фильтре Блюма пройдена, осуществить поиск по
-        // данным с жётского диска
+        // Если хотя бы один из элементов фильтра Блюма, соответствующий данному
+        // ключу равен нулю, то этого элемента нет среди данных
 
-        // Проверим, подпадает ли искомый ключ в диапазон ключей файла
-        auto borders = file_system_data[i];
-        if (key >= borders.first && key <= borders.second) {
-          size_t value_idx;
-          if (is_bin_search) {
-            value_idx = search_bin(i, key);
-          } else {
-            value_idx = search_linear(i, key);
-          }
-          string filename = get_filename(i, true);
-          fstream f(filename, ios::in | ios::binary);
-          f.seekg(value_idx * sizeof(Value_T));
-          Value_T* value = new Value_T[value_size];
-          f.read((char *)value, sizeof(Value_T)*value_size);
-          for (int i = 0; i < value_size; i++) {
-            if (value[i] != NULL) {
-              return value;
+        flag = bloom_filters[i].possibly_contains(key);
+
+        if (flag) {
+          // ----
+          // Если проверка в фильтре Блюма пройдена, осуществить поиск по
+          // данным с жётского диска
+
+          // Проверим, подпадает ли искомый ключ в диапазон ключей файла
+          auto borders = file_system_data[i];
+          if (key >= borders.first && key <= borders.second) {
+            int value_idx;
+            if (is_bin_search) {
+              value_idx = search_bin(i, key);
+            } else {
+              value_idx = search_linear(i, key);
             }
-          }
-          continue;
+            if (value_idx == -1) continue;
+
+            string filename = get_filename(i, true);
+            fstream f(filename, ios::in | ios::binary);
+            f.seekg(value_idx * sizeof(Value_T));
+            Value_T *value = new Value_T[value_size];
+            f.read((char *)value, sizeof(Value_T) * value_size);
+            for (int i = 0; i < value_size; i++) {
+              if (value[i] != NULL) {
+                return value;
+              }
+            }
+            continue;
+          } else
+            continue;
         } else
           continue;
-      } else
-        continue;
-    }
-    return NULL;
+      }
+      return nullptr;
+    } else {
+      int value_idx;
+      if (is_bin_search) {
+        value_idx = search_bin(0, key);
+      } else {
+        value_idx = search_linear(0, key);
+      }
 
+      if (value_idx == -1) return nullptr;
+
+      string filename = get_filename(0, true);
+      fstream f(filename, ios::in | ios::binary);
+      f.seekg(value_idx * sizeof(Value_T));
+      Value_T *value = new Value_T[value_size];
+      f.read((char *)value, sizeof(Value_T) * value_size);
+      return value;
+    }
   } else {
     return elem->second;
   }
@@ -229,6 +338,7 @@ Value_T* LSMTree<Key_T, Value_T>::get(Key_T key) {
 
 template <typename Key_T, typename Value_T>
 bool LSMTree<Key_T, Value_T>::is_memory_limit() {
-  return (mem_table.size() + 1) * (sizeof(Key_T) + sizeof(Value_T)*value_size) >=
+  return (mem_table.size() + 1) *
+             (sizeof(Key_T) + sizeof(Value_T) * value_size) >=
          THRESHOLD;
 }
--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <random>
 #include <string>
 #include <unordered_map>
+#include <algorithm>
 
 #include "LSMTree.h"
 
@@ -15,6 +16,7 @@ using namespace std;
 #define VALUE_TYPE unsigned int
 
 int main() {
+  
   const KEY_TYPE KEYS_ADD = 100000000;
   const KEY_TYPE KEYS_MAX = 1000000000;
   const VALUE_TYPE VALUE_MAX = numeric_limits<VALUE_TYPE>::max();
@@ -23,10 +25,21 @@ int main() {
   const bool BIN_SEARCH = true;
   random_device rd;
   default_random_engine generator(rd());
-  std::uniform_int_distribution<KEY_TYPE> key_distribution(1, KEYS_MAX);
   std::uniform_int_distribution<VALUE_TYPE> value_distribution(1, VALUE_MAX);
+  std::uniform_int_distribution<KEY_TYPE> key_distribution(1, KEYS_MAX);
 
-  LSMTree<KEY_TYPE, VALUE_TYPE> lsm(VALUE_ARRAY_LENGTH, BLOOM_FILTER, BIN_SEARCH);
+  // Psuedo random number generator
+  std::mt19937 prng(rd());
+
+  vector<KEY_TYPE> keys;
+  for (KEY_TYPE i = 1; i < KEYS_MAX; i++) {
+    keys.push_back(i);
+  }
+
+  std::shuffle(keys.begin(), keys.end(), prng);
+
+  LSMTree<KEY_TYPE, VALUE_TYPE> lsm(VALUE_ARRAY_LENGTH, BLOOM_FILTER,
+  BIN_SEARCH);
 
   auto add_start = clock();
   for (VALUE_TYPE i = 1; i <= KEYS_ADD; i++) {
@@ -34,22 +47,46 @@ int main() {
     for (int i = 0; i < VALUE_ARRAY_LENGTH; i++) {
       tmp[i] = value_distribution(generator);
     }
-    lsm.add(key_distribution(generator), tmp);
+    lsm.add(keys[i], tmp);
   }
   auto add_end = clock();
   cout << "Adding time (s): " << (double)(add_end - add_start) / CLOCKS_PER_SEC
        << endl;
 
+  keys.clear();
+
   auto get_start = clock();
   for (int i = 1; i <= 100000; i++) {
     VALUE_TYPE* a = lsm.get(key_distribution(generator));
   }
+
   auto get_end = clock();
   cout << "Getting elements time (s): "
        << (double)(get_end - get_start) / CLOCKS_PER_SEC << endl;
-  
 
-    
+
+
+
+  
+  /*
+  LSMTree<KEY_TYPE, VALUE_TYPE> lsm(1, false, true);
+  for (KEY_TYPE i = 0; i < 3000000; i++) {
+    unsigned int *a = new unsigned int(i + 1);
+    lsm.add(i, a);
+  }
+
+  cout << "ADDING DONE" << endl;
+
+  for (KEY_TYPE i = 0; i < 3000000; i++) {
+    unsigned int *a = lsm.get(i);
+    if (a[0] != i + 1) {
+      cout << "WRONG VALUE ON INDX " << i << endl;
+    }
+  }
+
+  cout << "GETTING DONE" << endl;
+  */
+
   /*int a[2] = {1, 2};
 
 fstream fout("test.bin", ios::out | ios::binary);
